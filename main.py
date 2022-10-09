ea616c94d02405e64de44481a1837d684d318d28
Danzan <108413140+DanzanShoinjonov@users.noreply.github.com>
2022-10-09 14:31:25 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from email import message
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5502461822:AAHk_yWHkTsqz3xyo5WgAELYOlgmESpO_78'
+
+logging.basicConfig(level=logging.INFO)
+
+bot= Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply('Привет!\nЯ "ЭхоБот".\nЯ работаю на "aiogram"\nМой создатель "Д Ш"')
+
+@dp.message_handler(commands=['go'])
+async def send_message(message: types.Message):
+    await message.reply('Я буду повторять за тобой')
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
